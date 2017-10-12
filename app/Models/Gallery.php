390b60ffecc66e1b172069cc758127ecbc51cb62
Max Kolyadin <makcyxa-k@yandex.ru>
2017-10-12 17:52:41 +0300

    before & after position change

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -75,7 +75,7 @@ class Gallery extends Model
                 // x = 450 | 550 - (240/2)
                 $img->overlay(public_path() . '/img/watermark/before.png', 'top left', 1, 760, 790);
                 // x = 1550 | 1650 - (240/2)
-                $img->overlay(public_path() . '/img/watermark/after.png', 'top left', 1, 1170, 790);
+                $img->overlay(public_path() . '/img/watermark/after.png', 'top left', 1, 1165, 790);
             }
 
 
