a4b7b081620bcc4815e4fc609e6c1c5633e33427
Max Kolyadin <makcyxa-k@yandex.ru>
2017-11-01 12:37:07 +0300

    Collapse price items

--- resources/views/directives/price-item.blade.php
+++ resources/views/directives/price-item.blade.php
@@ -1,7 +1,7 @@
+<a ng-show='item.is_section && item.items.length' class="pointer" style='margin-right: 3px'><i  class="pointer fa" ng-class="{'fa-plus': controller_scope.isCollapsed(item), 'fa-minus': !controller_scope.isCollapsed(item)}" ng-click="controller_scope.toggleCollapse(item)" aria-hidden="true"></i></a>
 <a ng-class="{'price-section': item.is_section}" href='prices/@{{ item.is_section ? ""  : "positions/" }}@{{ item.model.id }}/edit'>@{{ item.model.name }}</a>
 <a ng-show='item.is_section' href='prices/@{{ item.model.id }}/positions/create' class='pull-right'>+ позиция</a>
 <a ng-show='item.is_section' href='prices/@{{ item.model.id }}/create' class='pull-right' style='margin-right: 20px'>+ раздел</a>
-<span ng-show='item.is_section'><a ng-hide="!item.items.length" class='pull-right pointer' style='margin-right: 20px' ng-click="controller_scope.toggleCollapse(item)">@{{ controller_scope.isCollapsed(item) ? '+ развернуть' : '- свернуть' }}</a></span>
 <span class="pull-right" ng-show="!item.is_section && item.model.price" style='width: 600px'>
     от @{{ item.model.price | number }} руб.<span ng-show="item.model.unit">/@{{ findById(Units, item.model.unit).title }}</span>
 </span>
