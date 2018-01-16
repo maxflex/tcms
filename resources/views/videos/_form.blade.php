9ec7d4fbf59555beb3a8b951a6181582c2105037
Max Kolyadin <makcyxa-k@yandex.ru>
2018-01-16 15:02:45 +0300

    delete description

--- resources/views/videos/_form.blade.php
+++ resources/views/videos/_form.blade.php
@@ -6,15 +6,3 @@
         @include('modules.input', ['title' => 'код', 'model' => 'code'])
     </div>
 </div>
-<div class="row mb">
-    <div class="col-sm-12">
-        @include('modules.input', [
-            'title' => 'описание',
-            'model' => 'description',
-            'textarea' => true,
-            'attributes' => [
-                'maxlength' => 500
-            ]
-        ])
-    </div>
-</div>
