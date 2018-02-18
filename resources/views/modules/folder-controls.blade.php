022fec1a9be2703a261b6902d7b364f14c47184b
Max Kolyadin <makcyxa-k@yandex.ru>
2018-02-18 15:12:51 +0300

    Folder delete update

--- resources/views/modules/folder-controls.blade.php
+++ resources/views/modules/folder-controls.blade.php
@@ -1,5 +1,5 @@
 <span ng-show="!FolderService.folders.length && !IndexService.page.data.length" ng-click="FolderService.delete()">удалить папку</span>
-<span ng-show="!FolderService.current_folder_id" ng-click="FolderService.editModal()">редактировать</span>
+<span ng-show="FolderService.current_folder_id" ng-click="FolderService.editModal()">редактировать</span>
 <span ng-click="FolderService.createModal()">создать папку</span>
 
 <style>
