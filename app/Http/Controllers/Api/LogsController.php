6bbdec3d4204334736aa7672ac2e168605d18dfb
Max Kolyadin <makcyxa-k@yandex.ru>
2018-02-18 14:34:28 +0300

    logs pagination

--- app/Http/Controllers/Api/LogsController.php
+++ app/Http/Controllers/Api/LogsController.php
@@ -20,7 +20,7 @@ class LogsController extends Controller
     public function index()
     {
         $search = isset($_COOKIE['logs']) ? json_decode($_COOKIE['logs']) : (object)[];
-        $data = Log::search($search)->paginate(30);
+        $data = Log::search($search)->paginate(200);
         $data->getCollection()->map(function ($log) {
             if (in_array($log->table, ['attachments', 'archives', 'clients', 'request_lists', 'tutors']) && $log->type != 'delete') {
                 switch ($log->table) {
