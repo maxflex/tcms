310669274c64c1379bd1c2ef937bfcac874694d2
maxflex <makcyxa-k@yandex.ru>
2018-09-13 17:48:00 +0300

    allowed tag added

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    const ALLOWED_TAGS = ['p', 'br', 'ul', 'li', 'a'];
+    const ALLOWED_TAGS = ['p', 'br', 'ul', 'li', 'a', 'h3'];
     protected $visible = ['id', 'text'];
     protected $fillable = ['text', 'position'];
 }
