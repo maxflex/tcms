cc5f439d781a42f17582d54c5e778d011c5213d3
maxflex <>
2019-05-21 01:01:32 +0300

    Video image cdn

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\HasTags;
+use Storage;
 
 class Video extends Model
 {
@@ -11,4 +12,13 @@ class Video extends Model
 
     protected $fillable = ['code', 'title', 'tags', 'master_id', 'position', 'duration'];
     protected $appends = ['tags'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saved(function ($model) {
+            Storage::disk('spaces')->put('/img/video/' . $model->id . '.jpg', file_get_contents('https://img.youtube.com/vi/' . $model->code . '/mqdefault.jpg'));
+        });
+    }
 }
---
+++ config/do.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'spaces-endpoint-id' => env('DO_SPACES_ENDPOINT_ID'),
+    'api-key' => env('DO_API_KEY'),
+];
