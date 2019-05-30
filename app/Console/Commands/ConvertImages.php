b59692609da93423203e230cf17ecc7cd917eeba
maxflex <>
2019-05-31 02:34:06 +0300

    update

--- app/Console/Commands/ConvertImages.php
+++ app/Console/Commands/ConvertImages.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use WebPConvert\WebPConvert;
 use App\Models\Gallery;
+use App\Models\Master;
+use App\Models\Photo;
 
 class ConvertImages extends Command
 {
@@ -40,22 +42,38 @@ class ConvertImages extends Command
     public function handle()
     {
         $image = new \claviska\SimpleImage();
-        $ids = Gallery::where('id', '>', 0)->pluck('id');
-        $bar = $this->output->createProgressBar(count($ids));
-        foreach($ids as $id) {
-            $source = public_path() . '/img/gallery/' . $id . '.jpg';
-            $thumb = public_path() . '/img/gallery/' . $id . '_thumb.jpg';
-            $image
-                ->fromFile($source)
-                ->resize(288 * 2, 144 * 2)
-                ->toFile($thumb , 'image/jpeg', 90);
+        // $ids = Gallery::where('id', '>', 0)->pluck('id');
+        // $bar = $this->output->createProgressBar(count($ids));
+        // foreach($ids as $id) {
+        //     $source = public_path() . '/img/gallery/' . $id . '.jpg';
+        //     $thumb = public_path() . '/img/gallery/' . $id . '_thumb.jpg';
+        //     $image
+        //         ->fromFile($source)
+        //         ->resize(288 * 2, 144 * 2)
+        //         ->toFile($thumb , 'image/jpeg', 90);
 
-            $destination = public_path() . '/img/gallery/' . $id . '.webp';
-            WebPConvert::convert($source, $destination);
+        //     $destination = public_path() . '/img/gallery/' . $id . '.webp';
+        //     WebPConvert::convert($source, $destination);
 
-            $destination = public_path() . '/img/gallery/' . $id . '_thumb.webp';
-            WebPConvert::convert($thumb, $destination);
+        //     $destination = public_path() . '/img/gallery/' . $id . '_thumb.webp';
+        //     WebPConvert::convert($thumb, $destination);
 
+        //     $bar->advance();
+        // }
+        // $bar->finish();
+
+        // convert master images
+        $masters = Master::all();
+        $bar = $this->output->createProgressBar(count($masters));
+        foreach($masters as $master) {
+            foreach($master->photos as $photo) {
+                if ($photo->cropped && file_exists($photo->getFullPath())) {
+                    $image
+                        ->fromFile($photo->getFullPath())
+                        ->resize(420, null)
+                        ->toFile(Photo::getDir('small') . $photo->id . '.jpg', 'image/jpeg', 80);
+                }
+            }
             $bar->advance();
         }
         $bar->finish();
