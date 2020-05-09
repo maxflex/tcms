a6c51f76dfc13b4fd0342d7ff9eaf515dff580e6
maxflex <makcyxa-k@yandex.ru>
2020-05-09 16:56:34 +0300

    Remove limits

--- resources/views/galleries/_component.blade.php
+++ resources/views/galleries/_component.blade.php
@@ -1,6 +1,6 @@
 <div class="row mb">
     <div class="col-sm-9">
-        @include('modules.input', ['title' => 'компонент ' . $number, 'model' => 'component_' . $number, 'attributes' => ['maxlength' => 45]])
+        @include('modules.input', ['title' => 'компонент ' . $number, 'model' => 'component_' . $number, 'attributes' => in_array(userIdOrSystem(), [1, 3]) ? [] : ['maxlength' => 45]])
     </div>
     <div class="col-sm-3">
         @include('modules.input', ['title' => 'стоимость, руб.', 'model' => 'price_' . $number])
