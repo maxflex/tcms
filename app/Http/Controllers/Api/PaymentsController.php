53337a11942b7aaf46a838af66638c1c0852d842
maxflex <makcyxa-k@yandex.ru>
2020-05-18 23:46:52 +0300

    Remainder color

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -248,7 +248,7 @@ class PaymentsController extends Controller
                 $remainder += Payment::where('source_id', $source->id)->where('date', '>=', $date)->where('date', '<', $source->remainder_date)->sum('sum');
             }
             // если date == source->remainder_date, то будет перезаписано ниже
-            $totals[$date] = ['sum' => round($remainder, 2)];
+            $totals[$date] = ['sum' => round($remainder, 2), 'comment' => 'остаток на конец дня'];
         }
 
         // inject входящий остаток
