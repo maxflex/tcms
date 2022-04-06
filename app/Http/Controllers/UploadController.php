b75b8a464eaccbac2b15a9c08a942753d1566f5e
maxflex <makcyxa-k@yandex.ru>
2022-04-06 18:33:43 +0300

    Get client size removed

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -11,15 +11,15 @@ use App\Models\Photo;
 class UploadController extends Controller
 {
     const OK_FACTOR = 50;
-    const allowedMimeTypes = ['image/jpeg','image/jpg','image/png'];
+    const allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png'];
 
     public function galleryOriginal(Request $request)
     {
-        if ($request->file('photo')->getClientSize() > 7654604) { // 7.3 mb с запасом
-            return response()->json(['error' => 'максимальный объём файла – 7 Мб']);
-        }
+        // if ($request->file('photo')->getClientSize() > 7654604) { // 7.3 mb с запасом
+        //     return response()->json(['error' => 'максимальный объём файла – 7 Мб']);
+        // }
 
-        if (! in_array($request->file('photo')->getClientMimeType(), self::allowedMimeTypes)) {
+        if (!in_array($request->file('photo')->getClientMimeType(), self::allowedMimeTypes)) {
             return response()->json(['error' => 'разрешенные форматы – jpg, png']);
         }
 
@@ -38,7 +38,7 @@ class UploadController extends Controller
 
         $request->file('photo')->move(Photo::getDir('originals'), $filename);
 
-        if ( isset($request->photo_id)) {
+        if (isset($request->photo_id)) {
             // todo: удаление старого фото
             $photo = Photo::find($request->photo_id);
             $photo->update(['original' => $filename]);
@@ -59,7 +59,7 @@ class UploadController extends Controller
         }
         $filename = uniqid() . '.' . $request->file('photo')->getClientOriginalExtension();
         $request->file('photo')->move(Photo::getDir('originals'), $filename);
-        if ( isset($request->photo_id)) {
+        if (isset($request->photo_id)) {
             // todo: удаление старого фото
             $photo = Photo::find($request->photo_id);
             $photo->update(['original' => $filename]);
@@ -84,9 +84,9 @@ class UploadController extends Controller
 
         if ($request->watermark) {
             $positionY = 250;
-            foreach(range(1, 2) as $i) {
+            foreach (range(1, 2) as $i) {
                 $positionX = 100;
-                foreach(range(1, 4) as $j) {
+                foreach (range(1, 4) as $j) {
                     $image->overlay(public_path() . '/img/watermark/watermark.png', 'top left', .4, $positionX, $positionY);
                     $positionX += 560;
                 }
